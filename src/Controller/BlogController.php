cae762e686c9eb9a8984503524292159625c06d0
Quentin B <quentin.bisiaux@gmail.com>
2019-05-13 17:24:10 +0200

    function show added

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog", name="index")
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/blog", name="blog_index")
+     * @return Response
      */
     public function index()
     {
@@ -19,4 +20,20 @@ class BlogController extends AbstractController
             ]);
     }
 
+    /**
+     * @param $slug
+     * @Route("/blog/show/{slug<[a-z\d\-]+>}", name="blog_show")
+     * @return Response
+     */
+    public function show($slug = null)
+    {
+        if ($slug === null){
+            $title = 'Article Sans Titre';
+        }
+        $title = ucwords(str_replace("-", " ", $slug));
+
+        return $this->render('blog/show.html.twig', [
+            'slug' => $title,
+        ]);
+    }
 }
