307dd887280cb22a31b22dd9a3585d919788a593
Quentin B <quentin.bisiaux@gmail.com>
2019-05-14 15:09:31 +0200

    Display of articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,25 +16,60 @@ class BlogController extends AbstractController
      */
     public function index()
     {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
         return $this->render('blog/index.html.twig', [
-            'owner' => 'Quentin',
+            'articles' => $articles,
             ]);
     }
 
     /**
-     * @param $slug
-     * @Route("/blog/show/{slug<[a-z\d\-]+>}", name="blog_show")
-     * @return Response
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/blog/show/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     *  @return Response A response instance
      */
-    public function show($slug = null)
+    public function show(?string $slug) : Response
     {
-        if ($slug === null){
-            $title = 'Article Sans Titre';
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
         }
-        $title = ucwords(str_replace("-", " ", $slug));
 
-        return $this->render('blog/show.html.twig', [
-            'slug' => $title,
-        ]);
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
     }
+
 }
